34b7c1efbe59a8e5e7486ea7b676be17245b501d
Jorgen Lundman <lundman@lundman.net>
2021-07-07 19:32:30 +0900

    Upstream: let cache file to be 0-len

--- cmd/zdb/zdb.c
+++ cmd/zdb/zdb.c
@@ -4001,6 +4001,11 @@ dump_cachefile(const char *cachefile)
 		exit(1);
 	}
 
+	if (statbuf.st_size == 0) {
+		(void)close(fd);
+		return;
+	}
+
 	if ((buf = malloc(statbuf.st_size)) == NULL) {
 		(void) fprintf(stderr, "failed to allocate %llu bytes\n",
 		    (u_longlong_t)statbuf.st_size);
