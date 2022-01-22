af917941c8eaf2f8cc725b04385fc1c4a9c4d8db
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-01-22 01:56:46 +0100

    Forbid b{copy,zero,cmp}(). Don't include <strings.h> for <string.h>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Closes #12996

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -50,7 +50,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <time.h>
 #include <unistd.h>
 #include <pwd.h>
--- include/os/windows/spl/strings.h
+++
@@ -1,4 +0,0 @@
-#ifndef _SPL_STRINGS_H
-#define	_SPL_STRINGS_H
-
-#endif /* SPL_STRINGS_H */
