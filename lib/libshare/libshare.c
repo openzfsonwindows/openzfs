a5186554cc26baaa365f0f24b0ed904244f993a4
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-02-28 13:37:06 +0100

    libshare: interface: {=> const} char *
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Closes #13165

--- lib/libshare/libshare.c
+++ lib/libshare/libshare.c
@@ -84,7 +84,7 @@ libshare_init(void)
 
 int
 sa_enable_share(const char *zfsname, const char *mountpoint,
-    const char *shareopts, char *protocol)
+    const char *shareopts, const char *protocol)
 {
 	int rc, ret = SA_OK;
 	boolean_t found_protocol = B_FALSE;
@@ -118,7 +118,7 @@ sa_enable_share(const char *zfsname, const char *mountpoint,
 }
 
 int
-sa_disable_share(const char *mountpoint, char *protocol)
+sa_disable_share(const char *mountpoint, const char *protocol)
 {
 	int rc, ret = SA_OK;
 	boolean_t found_protocol = B_FALSE;
@@ -147,7 +147,7 @@ sa_disable_share(const char *mountpoint, char *protocol)
 }
 
 boolean_t
-sa_is_shared(const char *mountpoint, char *protocol)
+sa_is_shared(const char *mountpoint, const char *protocol)
 {
 	sa_fstype_t *fstype;
 	boolean_t ret = B_FALSE;
@@ -184,7 +184,7 @@ sa_commit_shares(const char *protocol)
  *
  * convert an error value to an error string
  */
-char *
+const char *
 sa_errorstr(int err)
 {
 	static char errstr[32];
@@ -302,7 +302,7 @@ sa_errorstr(int err)
 }
 
 int
-sa_validate_shareopts(char *options, char *proto)
+sa_validate_shareopts(const char *options, const char *proto)
 {
 	sa_fstype_t *fstype;
 
