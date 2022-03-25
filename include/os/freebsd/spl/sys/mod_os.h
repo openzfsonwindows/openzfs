6ccdf9614d273a92f0dcf6982135441456ce1bd2
Jorgen Lundman <lundman@lundman.net>
2022-03-25 10:13:26 +0900

    Upstream: Replace ZFS_MODULE_PARAM 'long' usage
    Unortunately, Windows defines 'long' as 32-bit even on x64
    compiles. We create two new macros ZFS_MODULE_LONG
    and ZFS_MODULE_ULONG. These two will be 'long' on Unix, and
    let the toolchain handle the size of it.
    
    On Windows the two macros are defined as 'int64_t'/'uint64_t'.

--- include/os/freebsd/spl/sys/mod_os.h
+++ include/os/freebsd/spl/sys/mod_os.h
@@ -35,6 +35,10 @@
 #define	module_param(a, b, c)
 #define	MODULE_PARM_DESC(a, b)
 
+/* Other platforms have long as 32-bit */
+#define	ZFS_MODULE_LONG long
+#define	ZFS_MODULE_ULONG unsigned long
+
 #define	ZMOD_RW CTLFLAG_RWTUN
 #define	ZMOD_RD CTLFLAG_RDTUN
 
