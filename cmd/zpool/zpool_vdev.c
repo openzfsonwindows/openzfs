6c3ad461250fd7197dbd9a837091354b2673bcb6
Jorgen Lundman <lundman@lundman.net>
2022-05-25 16:03:56 +0900

    Revert "Windows: partition disks win way and dont zero label after"
    This reverts commit 64dbc188d23fe744b7445dd0e229a05c30db4c9f.

--- cmd/zpool/zpool_vdev.c
+++ cmd/zpool/zpool_vdev.c
@@ -1025,10 +1025,6 @@ make_disks(zpool_handle_t *zhp, nvlist_t *nv)
 		} else {
 			(void) close(fd);
 		}
-#ifdef _WIN32
-		/* Dont really have partitions yet, so dont zero_label() */
-		is_exclusive = 1;
-#endif
 
 		/*
 		 * If the partition exists, contains a valid spare label,
