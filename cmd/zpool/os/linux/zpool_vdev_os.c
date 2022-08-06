e75deccf3c442c638584c32b42e3db92b820af75
Andrew Innes <andrew.c12@gmail.com>
2022-08-06 19:09:11 +0800

    remove duplicate check_file

--- cmd/zpool/os/linux/zpool_vdev_os.c
+++ cmd/zpool/os/linux/zpool_vdev_os.c
@@ -417,9 +417,3 @@ check_file(const char *file, boolean_t force, boolean_t isspare)
 {
 	return (check_file_generic(file, force, isspare));
 }
-
-int
-check_file(const char *file, boolean_t force, boolean_t isspare)
-{
-	return (check_file_generic(file, force, isspare));
-}
