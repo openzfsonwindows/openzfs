openzfs/zfs

Upstream: Use of posix_memalign need special free
Invent a posix_memalign_free() which is a wrapper to free() on
most platforms. Insert that wrapper into this commit

lib/libzutil/zutil_import.c
@@ -948,7 +948,9 @@ zpool_read_label(int fd, nvlist_t **config, int *num_labels)
 	if (num_labels != NULL)
 		*num_labels = count;
 
-	free(label);
+	/* Windows has special aligned free, let's invent a complementary free to memalign */
+	posix_memalign_free(label);
+
 	*config = expected_config;
 
 	return (0);

openzfsonwindows/openzfs

lib/libzutil/zutil_import.c
@@ -1105,7 +1105,8 @@ zpool_read_label(int fd, nvlist_t **config, int *num_labels)
 	if (num_labels != NULL)
 		*num_labels = count;
 
-	free(labels);
+	posix_memalign_free(labels);
+
 	*config = expected_config;
 
 	return (0);