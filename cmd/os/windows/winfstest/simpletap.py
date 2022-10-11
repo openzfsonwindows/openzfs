2a5c359545f701d3ae0f4a98a007c2212f80488f
Andrew Innes <andrew.c12@gmail.com>
2022-10-11 19:12:54 +0800

    Migrate to python3
    Signed-off-by: Andrew Innes <andrew.c12@gmail.com>

--- cmd/os/windows/winfstest/simpletap.py
+++ cmd/os/windows/winfstest/simpletap.py
@@ -62,7 +62,7 @@ def parse(iter):
     if pl is None:
         yield ("RR", "?", ok, ko)
     else:
-        yield ("RR", set(xrange(pl[0], pl[1])) - ok - ko, ok, ko)
+        yield ("RR", set(range(pl[0], pl[1])) - ok - ko, ok, ko)
 
 if "__main__" == __name__:
     import os, subprocess, sys
@@ -144,8 +144,8 @@ if "__main__" == __name__:
 
     try:
         main()
-    except subprocess.CalledProcessError, ex:
-        print >>sys.stderr
-        print >>sys.stderr, ex
-        print >>sys.stderr, ex.output
+    except subprocess.CalledProcessError as ex:
+        print(file=sys.stderr)
+        print(ex, file=sys.stderr)
+        print(ex.output, file=sys.stderr)
         sys.exit(ex.returncode)
