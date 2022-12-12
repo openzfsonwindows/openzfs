3f43fd7c2e1cc40c3c48ce093bb450b112baabe0
Ethan Coe-Renner <coerenner1@llnl.gov>
2022-12-12 15:30:51 -0800

    Add color output to zfs diff.
    This adds support to color zfs diff (in the style of git diff)
    conditional on the ZFS_COLOR environment variable.
    
    Signed-off-by: Ethan Coe-Renner <coerenner1@llnl.gov>

--- lib/libzfs/libzfs_util.c
+++ lib/libzfs/libzfs_util.c
@@ -2011,15 +2011,20 @@ use_color(void)
 void
 color_start(const char *color)
 {
-	if (use_color())
+	if (use_color()) {
 		fputs(color, stdout);
+		fflush(stdout);
+	}
 }
 
 void
 color_end(void)
 {
-	if (use_color())
+	if (use_color()) {
 		fputs(ANSI_RESET, stdout);
+		fflush(stdout);
+	}
+
 }
 
 /* printf() with a color.  If color is NULL, then do a normal printf. */
