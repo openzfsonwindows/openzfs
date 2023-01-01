8fa51c542daec02b5d7cd7252d941d08e2240029
Charles Suh <charles.suh@gmail.com>
2023-01-01 02:27:42 -0800

    Windows: fix incorrect offset and length when using
    a full disk with zero partitions
    
    Signed-off-by: Charles Suh <charles.suh@gmail.com>

--- lib/libzpool/kernel.c
+++ lib/libzpool/kernel.c
@@ -813,12 +813,8 @@ random_get_pseudo_bytes(uint8_t *ptr, size_t len)
 int
 ddi_strtoull(const char *str, char **nptr, int base, u_longlong_t *result)
 {
-	(void) nptr;
-	char *end;
-
+	char *end = NULL;
 	*result = strtoull(str, &end, base);
-	if (*result == 0)
-		return (errno);
 	if (nptr != NULL)
 		*nptr = end;
 	return (0);
