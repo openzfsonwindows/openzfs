e51f5f5eb60cbdbf75e57fabfa9efb71b608bee5
Jorgen Lundman <lundman@lundman.net>
2023-01-20 12:16:00 +0900

    Add ioctl to fetch mountpoint for snapshots
    The mounted directories is still not correct, they do not
    get registered with moutmgr, so userland can not look up
    where they are mounted. Until this can be understood and fixed,
    there is now a new ioctl to call to retrieve the mountpoint.
    
    userland calls it in getmntany() to gather all filesystems.
    
    this should let "export -a" to work properly, as it must
    unmount snapshots before the dataset (as they are nested)
    which is done by sorting the mountpoints, longest first.
    
    There are still weird things we need to solve, in addition the
    includes headers are just weird.

--- lib/libspl/include/os/windows/sys/zfs_context_os.h
+++ lib/libspl/include/os/windows/sys/zfs_context_os.h
@@ -38,4 +38,12 @@ struct zfs_handle;
 extern void zfs_rollback_os(struct zfs_handle *zhp);
 extern void libzfs_macos_wrapfd(int *srcfd, boolean_t send);
 
+#define	FSCTL_ZFS_VOLUME_MOUNTPOINT CTL_CODE(FILE_DEVICE_UNKNOWN,
+    0x8ff, METHOD_BUFFERED, FILE_ANY_ACCESS)
+typedef struct
+{
+    int len;
+    WCHAR buffer[1]; // make this dynamic?
+} fsctl_zfs_volume_mountpoint_t;
+
 #endif
