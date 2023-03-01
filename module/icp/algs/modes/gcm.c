f5b00b5787e565db61a8f5acf4f2b34ea985d672
Jorgen Lundman <lundman@lundman.net>
2023-03-01 15:22:43 +0900

    Minor correction to make direct $16 to work
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- module/icp/algs/modes/gcm.c
+++ module/icp/algs/modes/gcm.c
@@ -2237,7 +2237,6 @@ gcm_mode_encrypt_contiguous_blocks_isalc(gcm_ctx_t *ctx, const uint8_t *data,
 		kfpu_begin();
 		(*(isalc_ops.igo_enc_update[impl][keylen]))(
 		    ctx, ct_buf, datap, bleft);
-
 		kfpu_end();
 
 #ifdef DEBUG_GCM_ASM
