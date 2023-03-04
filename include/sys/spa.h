25bf1ddf2a0d2972fd89fbf89725bbc446faa8c9
Richard Yao <richard.yao@alumni.stonybrook.edu>
2023-03-03 21:15:28 -0500

    Suppress Clang Static Analyzer warning about SNPRINTF_BLKPTR()
    Clang's static analyzer pointed out that if we can pass a -1 array index
    to copyname[copies] if there are no valid DVAs. This is an absurd
    situation, but it suggests that we are missing an assertion, so we add
    it.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Richard Yao <richard.yao@alumni.stonybrook.edu>
    Closes #14575

--- include/sys/spa.h
+++ include/sys/spa.h
@@ -663,6 +663,7 @@ _NOTE(CONSTCOND) } while (0)
 			    (u_longlong_t)DVA_GET_ASIZE(dva),		\
 			    ws);					\
 		}							\
+		ASSERT3S(copies, >, 0);					\
 		if (BP_IS_ENCRYPTED(bp)) {				\
 			len += func(buf + len, size - len,		\
 			    "salt=%llx iv=%llx:%llx%c",			\
