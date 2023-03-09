30729e7d965b4167c416653795524c4b9cd212e3
Jorgen Lundman <lundman@lundman.net>
2023-03-09 09:02:10 +0900

    Add cmn_err_once and update file

--- include/os/windows/spl/sys/cmn_err.h
+++ include/os/windows/spl/sys/cmn_err.h
@@ -3,11 +3,11 @@
  *
  * The contents of this file are subject to the terms of the
  * Common Development and Distribution License, Version 1.0 only
- * (the "License"). You may not use this file except in compliance
+ * (the "License").  You may not use this file except in compliance
  * with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
- * or http://www.opensolaris.org/os/licensing.
+ * or https://opensource.org/licenses/CDDL-1.0.
  * See the License for the specific language governing permissions
  * and limitations under the License.
  *
@@ -19,36 +19,42 @@
  *
  * CDDL HEADER END
  */
-/* Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T */
-/* All Rights Reserved */
-
-
-/*
- * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
- * Use is subject to license terms.
- *
- * Copyright 2012 Nexenta Systems, Inc. All rights reserved.
- */
 
 #ifndef _SPL_CMN_ERR_H
 #define	_SPL_CMN_ERR_H
 
 #include <stdarg.h>
-#include <sys/varargs.h>
+#include <sys/atomic.h>
 
-#define	CE_CONT		0	/* continuation		*/
-#define	CE_NOTE		1	/* notice		*/
-#define	CE_WARN		2	/* warning		*/
-#define	CE_PANIC	3	/* panic		*/
-#define	CE_IGNORE	4	/* print nothing	*/
+#define	CE_CONT		0 /* continuation */
+#define	CE_NOTE		1 /* notice */
+#define	CE_WARN		2 /* warning */
+#define	CE_PANIC	3 /* panic */
+#define	CE_IGNORE	4 /* print nothing */
 
-#ifdef _KERNEL
-
-extern void vcmn_err(int, const char *, __va_list);
-extern void cmn_err(int, const char *, ...);
-
-#endif /* _KERNEL */
+extern void cmn_err(int, const char *, ...)
+    __attribute__((format(printf, 2, 3)));
+extern void vcmn_err(int, const char *, va_list)
+    __attribute__((format(printf, 2, 0)));
+extern void vpanic(const char *, va_list)
+    __attribute__((format(printf, 1, 0), __noreturn__));
 
 #define	fm_panic	panic
+
+#define	cmn_err_once(ce, ...)				\
+{							\
+	static volatile uint32_t printed = 0;		\
+	if (atomic_cas_32(&printed, 0, 1) == 0) {	\
+		cmn_err(ce, __VA_ARGS__);		\
+	}						\
+}
+
+#define	vcmn_err_once(ce, fmt, ap)			\
+{							\
+	static volatile uint32_t printed = 0;		\
+	if (atomic_cas_32(&printed, 0, 1) == 0) {	\
+		vcmn_err(ce, fmt, ap);			\
+	}						\
+}
 
 #endif /* SPL_CMN_ERR_H */
