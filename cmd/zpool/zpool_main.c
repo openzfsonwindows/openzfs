592f6335e13606cc24cc3c4c68392d72a1f3f11a
Rob Norris <rob.norris@truenas.com>
2023-09-29 14:53:27 +1000

    log_spacemap: extend pool flushall to have "request" and "export" modes
    Normally, log spacemaps are flushed out to the metaslabs when the pool
    is exported. For large logs, this can lead to export taking an
    inordinate amount of time.
    
    This commit adds a "mode" parameter for the log spacemap "flushall"
    operation, and functions for starting and stopping it in a particular
    mode. The existing behaviour of flushing everything is now the "export"
    mode.
    
    Then, we add a new "request" mode, that can be triggered externally.
    This activates the same flushall code, with a few differences:
    
    - we only consider flushing metaslabs that were dirtied on the txg
      before the flushall operation was started
    
    - we close and issue the txg immediately when the flushall is active,
      rather than wait for zfs_txg_timeout each time (similar to how scrub
      also forces the txg forward)
    
    - each txg, we flush a fixed number of metaslabs, regardless of the rate
      of change. The amount is set by a new tunable new tunable
      zfs_min_metaslabs_to_condense_pct, which defaults to 5% of the number
      of unflushed metaslabs at the time the condense operation was started.
    
    The minimum dirty metaslabs pct% together with forcing the txg forward
    should mean that with the defaults, condensing is attempted at 5% per
    second, and so on an idle pool should take no more than 20s to flush.
    
    Sponsored-by: TrueNAS
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <rob.norris@truenas.com>
    Closes #16747

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -369,7 +369,8 @@ static const char *vdev_trim_state_str[] = {
 };
 
 static const char *condense_unit(const char *type) {
-	(void) type;
+	if (strcmp(type, POOL_CONDENSE_LOG_SPACEMAP) == 0)
+		return ("metaslabs");
 #ifdef ZFS_DEBUG
 	if (strcmp(type, "debug") == 0)
 		return ("nits");	/* fundamental unit of debugging? ;) */
@@ -8885,6 +8886,7 @@ zpool_do_trim(int argc, char **argv)
 }
 
 static const char *condense_types[] = {
+	POOL_CONDENSE_LOG_SPACEMAP,
 #ifdef ZFS_DEBUG
 	"debug",
 #endif
--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -1533,7 +1533,7 @@ typedef enum pool_trim_func {
 /*
  * Condense types.
  */
-/* ... */
+#define	POOL_CONDENSE_LOG_SPACEMAP	"log_spacemap"
 
 /*
  * DDT statistics.  Note: all fields should be 64-bit because this
