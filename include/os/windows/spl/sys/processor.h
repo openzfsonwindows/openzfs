rottegift/openzfs-fork

Redo cpuid feature detection
We already have a proper cpuid feature API used in
userland, but Linux took it out for KERNEL use, to
plug into the Linux API. We do not have a kernel API to
use, so we might as well use the userland one.

This makes it nice and consistent, and more easy to read.

Signed-off-by: Jorgen Lundman <lundman@lundman.net>

include/os/macos/spl/sys/processor.h
@@ -31,8 +31,53 @@
 #include <sys/types.h>
 
 extern uint32_t getcpuid(void);
-extern uint64_t spl_cpuid_features(void);
+
+#if defined(__amd64__) || defined(__i386__)
+
+extern int __cpuid_count(unsigned int __level, unsigned int __sublevel,
+	unsigned int __eax, unsigned int __ebx,
+	unsigned int __ecx, unsigned int __edx);
+
+#define	__cpuid_count(level, count, a, b, c, d) \
+	__asm__("xchg{l}\t{%%}ebx, %1\n\t" \
+		"cpuid\n\t" \
+		"xchg{l}\t{%%}ebx, %1\n\t" \
+		: "=a" (a), "=r" (b), "=c" (c), "=d" (d) \
+		: "0" (level), "2" (count))
+
+#define	__cpuid(level, a, b, c, d) \
+	__asm__("xchg{l}\t{%%}ebx, %1\n\t" \
+		"cpuid\n\t" \
+		"xchg{l}\t{%%}ebx, %1\n\t" \
+		: "=a" (a), "=r" (b), "=c" (c), "=d" (d) \
+		: "0" (level))
+
+static inline unsigned int
+__get_cpuid_max(unsigned int __ext, unsigned int *__sig)
+{
+	unsigned int __eax, __ebx, __ecx, __edx;
+	__cpuid(__ext, __eax, __ebx, __ecx, __edx);
+	if (__sig)
+		*__sig = __ebx;
+	return (__eax);
+}
+
+/* macOS does have do_cpuid() macro */
+static inline int
+__get_cpuid(unsigned int __level,
+    unsigned int *__eax, unsigned int *__ebx,
+    unsigned int *__ecx, unsigned int *__edx)
+{
+	unsigned int __ext = __level & 0x80000000;
+	if (__get_cpuid_max(__ext, 0) < __level)
+		return (0);
+	__cpuid(__level, *__eax, *__ebx, *__ecx, *__edx);
+	return (1);
+}
+
+#endif // x86
 
 typedef int	processorid_t;
+extern int spl_processor_init(void);
 
 #endif /* _SPL_PROCESSOR_H */

module/os/macos/spl/spl-processor.c
@@ -32,19 +32,7 @@ extern int cpu_number(void);
 #if defined(__x86_64__)
 
 #include <i386/cpuid.h>
-
-#define	_spl_cpuid(func, a, b, c, d)			\
-	__asm__ __volatile__( \
-	"        pushq %%rbx        \n" \
-	"        xorq %%rcx,%%rcx   \n" \
-	"        cpuid              \n" \
-	"        movq %%rbx, %%rsi  \n" \
-	"        popq %%rbx         \n" : \
-	"=a" (a), "=S" (b), "=c" (c), "=d" (d) : "a" (func))
-
-static uint64_t _spl_cpuid_features = 0ULL;
-static uint64_t _spl_cpuid_features_leaf7 = 0ULL;
-static boolean_t _spl_cpuid_has_xgetbv = B_FALSE;
+#include <sys/simd_x86.h>
 
 #elif defined(__aarch64__)
 
@@ -84,52 +72,6 @@ getcpuid(void)
 	return ((uint32_t)cpu_number());
 #endif
 }
-
-
-#if defined(__x86_64__)
-uint64_t
-spl_cpuid_features(void)
-{
-	static int first_time = 1;
-	uint64_t a, b, c, d;
-
-	if (first_time == 1) {
-		first_time = 0;
-		_spl_cpuid(0, a, b, c, d);
-		if (a >= 1) {
-			_spl_cpuid(1, a, b, c, d);
-			_spl_cpuid_features = d | (c << 32);
-
-			// GETBV is bit 26 in ECX. Apple defines it as:
-			// CPUID_FEATURE_XSAVE _HBit(26)
-			// ( ECX & (1 << 26)
-			// or, (feature & 400000000000000)
-			_spl_cpuid_has_xgetbv =
-			    _spl_cpuid_features & CPUID_FEATURE_XSAVE;
-		}
-		if (a >= 7) {
-			c = 0;
-			_spl_cpuid(7, a, b, c, d);
-			_spl_cpuid_features_leaf7 = b | (c << 32);
-		}
-
-
-		printf("SPL: CPUID 0x%08llx and leaf7 0x%08llx\n",
-		    _spl_cpuid_features, _spl_cpuid_features_leaf7);
-
-	}
-
-	return (_spl_cpuid_features);
-}
-
-uint64_t
-spl_cpuid_leaf7_features(void)
-{
-	return (_spl_cpuid_features_leaf7);
-}
-
-#endif /* x86_64 */
-
 
 #if defined(__aarch64__)
 
@@ -201,12 +143,35 @@ spl_cpuid_id_aa64isar1_el1(void)
 	return (_spl_cpuid_id_aa64isar1_el1);
 }
 
-uint64_t
-spl_cpuid_features(void)
+#endif /* aarch64 */
+
+int
+spl_processor_init(void)
 {
+
+#if defined(__aarch64__)
 	spl_cpuid_id_aa64isar0_el1();
 	spl_cpuid_id_aa64isar1_el1();
-	return (0ULL);
-}
+#endif
 
-#endif /* aarch64 */
+#if defined(__x86)
+	printf("CPUID: %s%s%s%s%s%s%s\n",
+	    zfs_osxsave_available() ? "osxsave " : "",
+	    zfs_sse_available() ? "sse " : "",
+	    zfs_sse2_available() ? "sse2 " : "",
+	    zfs_sse3_available() ? "sse3 " : "",
+	    zfs_ssse3_available() ? "ssse3 " : "",
+	    zfs_sse4_1_available() ? "sse4.1 " : "",
+	    zfs_sse4_2_available() ? "sse4.2 " : "");
+	printf("CPUID: %s%s%s%s%s%s%s\n",
+	    zfs_avx_available() ? "avx " : "",
+	    zfs_avx2_available() ? "avx2 " : "",
+	    zfs_aes_available() ? "aes " : "",
+	    zfs_pclmulqdq_available() ? "pclmulqdq " : "",
+	    zfs_avx512f_available() ? "avx512f " : "",
+	    zfs_movbe_available() ? "movbe " : "",
+	    zfs_shani_available() ? "sha-ni " : "");
+#endif
+
+	return (0);
+}

openzfsonwindows/openzfs

include/os/windows/spl/sys/processor.h
@@ -8,22 +8,52 @@ extern uint32_t getcpuid();
 
 typedef int	processorid_t;
 
-#define	CPUID_FEATURE_PCLMULQDQ		(1<<1)
-#define	CPUID_FEATURE_MOVBE		(1<<22)
-#define	CPUID_FEATURE_AES		(1<<25)
-#define	CPUID_FEATURE_XSAVE		(1<<26)
-#define	CPUID_FEATURE_OSXSAVE		(1<<27)
-#define	CPUID_FEATURE_AVX1_0		(1<<28)
+#if defined(__amd64__) || defined(__x86_64__)
 
-#define	CPUID_FEATURE_SSE		(1<<25)
-#define	CPUID_FEATURE_SSE2		(1<<26)
-#define	CPUID_FEATURE_SSE3		(1<<0)
-#define	CPUID_FEATURE_SSSE3		(1<<9)
-#define	CPUID_FEATURE_SSE4_2		(1<<20)
-#define	CPUID_FEATURE_SSE4_1		(1<<19)
+extern int __cpuid_count(unsigned int __level, unsigned int __sublevel,
+	unsigned int __eax, unsigned int __ebx,
+	unsigned int __ecx, unsigned int __edx);
 
-#define	CPUID_LEAF7_FEATURE_AVX2    (1<<5)
-#define	CPUID_LEAF7_FEATURE_AVX512F    (1<<16)
-#define	CPUID_LEAF7_FEATURE_SHA_NI		(1<<29)
+#define	__cpuid_count(level, count, a, b, c, d) \
+	__asm__("xchg{l}\t{%%}ebx, %1\n\t" \
+		"cpuid\n\t" \
+		"xchg{l}\t{%%}ebx, %1\n\t" \
+		: "=a" (a), "=r" (b), "=c" (c), "=d" (d) \
+		: "0" (level), "2" (count))
+
+#define	__cpuid(level, a, b, c, d) \
+	__asm__("xchg{l}\t{%%}ebx, %1\n\t" \
+		"cpuid\n\t" \
+		"xchg{l}\t{%%}ebx, %1\n\t" \
+		: "=a" (a), "=r" (b), "=c" (c), "=d" (d) \
+		: "0" (level))
+
+static inline unsigned int
+__get_cpuid_max(unsigned int __ext, unsigned int *__sig)
+{
+	unsigned int __eax, __ebx, __ecx, __edx;
+	__cpuid(__ext, __eax, __ebx, __ecx, __edx);
+	if (__sig)
+		*__sig = __ebx;
+	return (__eax);
+}
+
+/* macOS does have do_cpuid() macro */
+static inline int
+__get_cpuid(unsigned int __level,
+    unsigned int *__eax, unsigned int *__ebx,
+    unsigned int *__ecx, unsigned int *__edx)
+{
+	unsigned int __ext = __level & 0x80000000;
+	if (__get_cpuid_max(__ext, 0) < __level)
+		return (0);
+	__cpuid(__level, *__eax, *__ebx, *__ecx, *__edx);
+	return (1);
+}
+
+#endif // x86
+
+typedef int	processorid_t;
+extern int spl_processor_init(void);
 
 #endif /* _SPL_PROCESSOR_H */

module/os/windows/spl/spl-processor.c
@@ -26,33 +26,10 @@
  */
 
 #include <sys/processor.h>
+#include <sys/simd.h>
 
 /* Holds the flags for KeSaveExtendedProcessorState() in simd.h */
 uint32_t kfpu_state = 0;
-
-#ifdef __x86_64__
-
-/* Should probably use MS cpuid() call */
-
-#define	_spl_cpuid(func, a, b, c, d)	\
-	__asm__ __volatile__( \
-	"        pushq %%rbx        \n" \
-	"        xorq %%rcx,%%rcx   \n" \
-	"        cpuid              \n" \
-	"        movq %%rbx, %%rsi  \n" \
-	"        popq %%rbx         \n" : \
-	"=a" (a), "=S" (b), "=c" (c), "=d" (d) : "a" (func))
-
-#else /* Add ARM */
-
-#define	_spl_cpuid(func, a, b, c, d)				\
-	a = b = c = d = 0
-
-#endif
-
-static uint64_t _spl_cpuid_features = 0ULL;
-static uint64_t _spl_cpuid_features_leaf7 = 0ULL;
-static boolean_t _spl_cpuid_has_xgetbv = B_FALSE;
 
 uint32_t
 cpu_number(void)
@@ -70,58 +47,27 @@ getcpuid()
 	return (cpuid % max_ncpus);
 }
 
-uint64_t
-spl_cpuid_features(void)
+int
+spl_processor_init(void)
 {
-
-#if defined(__aarch64__)
-
-	// Find arm64 solution.
-	_spl_cpuid_has_xgetbv = B_FALSE; /* Silence unused */
-
-#else /* X64 */
-
-	static int first_time = 1;
-	uint64_t a, b, c, d;
-
-	if (first_time == 1) {
-		first_time = 0;
-		// Wikipedia: stored in EAX, EBX, EDX, ECX (in that order).
-		_spl_cpuid(0, a, b, d, c);
-		if (a >= 1) {
-			_spl_cpuid(1, a, b, d, c);
-			_spl_cpuid_features = d | (c << 32);
-
-			// GETBV is bit 26 in ECX. Apple defines it as:
-			// CPUID_FEATURE_XSAVE _HBit(26)
-			// ( ECX & (1 << 26)
-			// or, (feature & 400000000000000)
-			_spl_cpuid_has_xgetbv =
-			    _spl_cpuid_features & CPUID_FEATURE_XSAVE;
-		}
-		if (a >= 7) {
-			c = 0;
-			_spl_cpuid(7, a, b, d, c);
-			_spl_cpuid_features_leaf7 = b | (c << 32);
-		}
-		xprintf("SPL: CPUID 0x%08llx and leaf7 0x%08llx\n",
-		    _spl_cpuid_features, _spl_cpuid_features_leaf7);
-
-		if (_spl_cpuid_features & CPUID_FEATURE_AVX1_0)
-			kfpu_state |= XSTATE_MASK_AVX;
-		if (_spl_cpuid_features_leaf7 & CPUID_LEAF7_FEATURE_AVX2)
-			kfpu_state |= XSTATE_MASK_AVX;
-		if (_spl_cpuid_features_leaf7 & CPUID_LEAF7_FEATURE_AVX512F)
-			kfpu_state |= XSTATE_MASK_AVX512;
-
-	}
+#if defined(__x86_64__)
+	dprintf("CPUID: %s%s%s%s%s%s%s\n",
+	    zfs_osxsave_available() ? "osxsave " : "",
+	    zfs_sse_available() ? "sse " : "",
+	    zfs_sse2_available() ? "sse2 " : "",
+	    zfs_sse3_available() ? "sse3 " : "",
+	    zfs_ssse3_available() ? "ssse3 " : "",
+	    zfs_sse4_1_available() ? "sse4.1 " : "",
+	    zfs_sse4_2_available() ? "sse4.2 " : "");
+	dprintf("CPUID: %s%s%s%s%s%s%s\n",
+	    zfs_avx_available() ? "avx " : "",
+	    zfs_avx2_available() ? "avx2 " : "",
+	    zfs_aes_available() ? "aes " : "",
+	    zfs_pclmulqdq_available() ? "pclmulqdq " : "",
+	    zfs_avx512f_available() ? "avx512f " : "",
+	    zfs_movbe_available() ? "movbe " : "",
+	    zfs_shani_available() ? "sha-ni " : "");
 #endif
 
-	return (_spl_cpuid_features);
-}
-
-uint64_t
-spl_cpuid_leaf7_features(void)
-{
-	return (_spl_cpuid_features_leaf7);
+	return (0);
 }