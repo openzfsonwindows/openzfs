713750956835d1144a13de6aca236cedf1113b1d
Rob Norris <robn@despairlabs.com>
2024-12-04 17:49:09 +1100

    Remove unnecessary CSTYLED escapes on top-level macro invocations
    cstyle can handle these cases now, so we don't need to disable it.
    
    Sponsored-by: https://despairlabs.com/sponsor/
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <robn@despairlabs.com>
    Closes #16840

--- module/nvpair/nvpair.c
+++ module/nvpair/nvpair.c
@@ -3309,7 +3309,6 @@ nvs_xdr_nvp_##type(XDR *xdrs, void *ptr, ...)	\
 
 #endif
 
-/* BEGIN CSTYLED */
 NVS_BUILD_XDRPROC_T(char);
 NVS_BUILD_XDRPROC_T(short);
 NVS_BUILD_XDRPROC_T(u_short);
@@ -3317,7 +3316,6 @@ NVS_BUILD_XDRPROC_T(int);
 NVS_BUILD_XDRPROC_T(u_int);
 NVS_BUILD_XDRPROC_T(longlong_t);
 NVS_BUILD_XDRPROC_T(u_longlong_t);
-/* END CSTYLED */
 
 /*
  * The format of xdr encoded nvpair is:
