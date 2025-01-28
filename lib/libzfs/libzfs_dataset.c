0b430755c3a686a4b20a9a2ea86a5f92dad19e74
Ivan Shapovalov <intelfx@intelfx.name>
2025-01-28 12:11:18 +0400

    zfs_main: create, clone, rename: accept `-pp` for non-mountable parents
    Teach `zfs {create,clone,rename}` to accept a doubled `-p` flag (`-pp`)
    to create non-existing ancestor datasets with `canmount=off`.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Rob Norris <robn@despairlabs.com>
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>
    Closes #17000

--- lib/libzfs/libzfs_dataset.c
+++ lib/libzfs/libzfs_dataset.c
@@ -3707,7 +3707,7 @@ ancestorerr:
 int
 zfs_create_ancestors(libzfs_handle_t *hdl, const char *path)
 {
-	return zfs_create_ancestors_props(hdl, path, NULL);
+	return (zfs_create_ancestors_props(hdl, path, NULL));
 }
 
 /*
