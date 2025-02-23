ad290861540a11152ecf4881d34f000c62219c54
Jorgen Lundman <lundman@lundman.net>
2025-02-23 21:29:57 +0900

    Save cbuf on DriverUnload
    Clean up the two saveBuffer functions into one,
    do not call systemboot on saveBuffer (debug left in)
    
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- module/os/windows/driver.c
+++ module/os/windows/driver.c
@@ -61,6 +61,8 @@ extern void sysctl_os_init(PUNICODE_STRING RegistryPath);
 extern void sysctl_os_all(PUNICODE_STRING RegistryPath);
 extern void sysctl_os_fini(void);
 
+extern void saveBuffer(void);
+
 
 #ifdef __clang__
 #error "This file should be compiled with MSVC not Clang"
@@ -87,6 +89,9 @@ OpenZFS_Fini(PDRIVER_OBJECT DriverObject)
 	system_taskq_fini();
 
 	spl_stop();
+
+	saveBuffer();
+
 #ifdef DBG
 	if (KD_DEBUGGER_ENABLED && !KD_DEBUGGER_NOT_PRESENT) {
 		xprintf("Breaking into debugger after unload\n");
@@ -96,6 +101,8 @@ OpenZFS_Fini(PDRIVER_OBJECT DriverObject)
 	finiDbgCircularBuffer();
 
 	ZFSWppCleanup(DriverObject);
+
+	// IoFreeDriverObjectExtension(DriverObject);
 
 	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL,
 	    "OpenZFS: Goodbye.\n"));
