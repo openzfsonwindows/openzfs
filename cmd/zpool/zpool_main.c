82f754360c897d59ff64658a58266e845f553647
Tim Hatch <tim@timhatch.com>
2026-02-09 10:19:08 -0800

    Include missing newline in 'man' error
    Because the `strerror` result doesn't include a newline, we need to add
    one.  Observed on a minimal system that doesn't have `man` installed,
    which behaves like this before the fix:
    
    ```
    [root@upper tim]# zpool help import
    couldn't run man program: No such file or directory[root@upper tim]#
    ```
    
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Tim Hatch <tim@timhatch.com>
    Closes #18183

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -13797,7 +13797,7 @@ zpool_do_help(int argc, char **argv)
 
 	(void) execlp("man", "man", page, NULL);
 
-	fprintf(stderr, "couldn't run man program: %s", strerror(errno));
+	fprintf(stderr, "couldn't run man program: %s\n", strerror(errno));
 	return (-1);
 }
 
