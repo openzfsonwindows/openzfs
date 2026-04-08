497670081959815b0139712f36ec33c5e43de835
Andriy Tkachuk <andriy.tkachuk@seagate.com>
2026-04-08 18:09:47 +0100

    draid: add failure domains support
    Currently, the only way to tolerate the failure of the whole
    enclosure is to configure several draid vdevs in the pool, each
    vdev having disks from different enclosures. But this essentially
    degrades draid to raidz and defeats the purpose of having fast
    sequential resilvering on wide pools with draid.
    
    This patch allows to configure several children groups in the
    same row in one draid vdev. In each such group, let's call it
    failure group, the user can configure disks belonging to different
    enclosures - failure domains. For example, in case of 10 such
    enclosures with 10 disks each, the user can put 1st disk from each
    enclosure into 1st group, 2nd disk from each enclosure into 2nd
    group, and so on. If one enclosure fails, only one disk from each
    group would fail, which won't affect draid operation, and each
    group would have enough redundancy to recover the stored data. Of
    course, in case of draid2 - two enclosures can fail at a time, in
    case of draid3 - three enclosures (provided there are no other
    disk failures in each group).
    
    In order to preserve fast sequential resilvering in case of a
    disk failure, the groups much share all disks between themselves,
    and this is achieved by shuffling the disks between the groups.
    But only i-th disks in each group are shuffled between themselves,
    i.e. the disks from the same enclosures, after that they are
    shuffled within each group, like it is done today in an ordinary
    draid. Thus, no more than one disk from any enclosure can appear
    in any failure group as a result of this shuffling.
    
    For example, here's how the pool status output looks like in
    case of two `draid1:2d:4c` failure groups:
    
        NAME                        STATE     READ WRITE CKSUM
        pool1                       ONLINE       0     0     0
          draid1:2d:4c:8w:1s-0      ONLINE       0     0     0
            enc0d0                  ONLINE       0     0     0
            enc1d0                  ONLINE       0     0     0
            enc2d0                  ONLINE       0     0     0
            enc3d0                  ONLINE       0     0     0
            enc0d1                  ONLINE       0     0     0
            enc1d1                  ONLINE       0     0     0
            enc2d1                  ONLINE       0     0     0
            enc3d1                  ONLINE       0     0     0
        spares
          draid1-0-0                AVAIL
    
    The number of failure groups is specified indirectly via the new
    width parameter in draid vdev configuration descriptor, which is
    the total number of disks and which is multiple of children in
    each group. This multiple is the number of groups (width /
    children). Doing it this way allows the user conveniently see how
    many disks draid has in an instant.
    
    Spare disks are evenly distributed among failure groups, and they
    are shared by all groups. However, to support domain failure, we
    cannot have more than nparity - 1 failed disks in any group, even
    if they are rebuilt to draid spares (the blocks of those spares
    can be mapped to the disks from the failed domain, and we cannot
    tolerate more than nparity failures in any failure group).
    
    The retire agent in zed is updated to not start resilvering when
    the domain failure happens. Otherwise, it might take a lot of
    computing and I/O bandwidth resources, only to be wasted when the
    failed domain component is replaced.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Akash B <akash-b@hpe.com>
    Signed-off-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>
    Closes #11969
    Closes #18148

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -3531,6 +3531,11 @@ show_import(nvlist_t *config, boolean_t report_error)
 		    "accessed by another system.\n"));
 		break;
 
+	case ZPOOL_STATUS_FAULTED_FDOM_R:
+		(void) printf_color(ANSI_YELLOW, gettext("One or more failure "
+		    " domains are faulted.\n"));
+		break;
+
 	case ZPOOL_STATUS_FAULTED_DEV_R:
 	case ZPOOL_STATUS_FAULTED_DEV_NR:
 		(void) printf_color(ANSI_YELLOW, gettext("One or more devices "
@@ -8078,7 +8083,7 @@ zpool_do_online(int argc, char **argv)
 
 	if ((zhp = zpool_open(g_zfs, poolname)) == NULL) {
 		(void) fprintf(stderr, gettext("failed to open pool "
-		    "\"%s\""), poolname);
+		    "\"%s\"\n"), poolname);
 		return (1);
 	}
 
@@ -8222,7 +8227,7 @@ zpool_do_offline(int argc, char **argv)
 
 	if ((zhp = zpool_open(g_zfs, poolname)) == NULL) {
 		(void) fprintf(stderr, gettext("failed to open pool "
-		    "\"%s\""), poolname);
+		    "\"%s\"\n"), poolname);
 		return (1);
 	}
 
@@ -10733,6 +10738,18 @@ print_status_reason(zpool_handle_t *zhp, status_cbdata_t *cbp,
 		(void) snprintf(action, AC_SIZE,
 		    gettext("Replace the faulted device, "
 		    "or use 'zpool clear' to mark the device\n\trepaired.\n"));
+		break;
+
+	case ZPOOL_STATUS_FAULTED_FDOM_R:
+		(void) snprintf(status, ST_SIZE,
+		    gettext("One or more failure domains are faulted. "
+		    "The storage devices may be\n\tintact. Sufficient "
+		    "replicas exist for the pool to continue functioning\n\t"
+		    "in a degraded state.\n"));
+		(void) snprintf(action, AC_SIZE,
+		    gettext("Replace the faulted domain device, "
+		    "or use 'zpool clear' to mark domain\n\tstorage devices "
+		    "repaired.\n"));
 		break;
 
 	case ZPOOL_STATUS_FAULTED_DEV_NR:
