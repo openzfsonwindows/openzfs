cb4a54f2bd58a16d33f5d2b9ecef117f3c1b34f8
Rob Norris <rob.norris@truenas.com>
2026-08-19 13:06:23 +1000

    vdev_file: use calling cred to check for device access
    Sponsored-by: TrueNAS
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Signed-off-by: Rob Norris <rob.norris@truenas.com>
    Closes #18960

--- module/zfs/vdev_file.c
+++ module/zfs/vdev_file.c
@@ -13,6 +13,7 @@
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2011, 2020 by Delphix. All rights reserved.
  * Copyright (c) 2025, Klara, Inc.
+ * Copyright (c) 2026, TrueNAS.
  */
 
 #include <sys/zfs_context.h>
@@ -89,7 +90,6 @@ static int
 vdev_file_open(vdev_t *vd, uint64_t *psize, uint64_t *max_psize,
     uint64_t *logical_ashift, uint64_t *physical_ashift, cred_t *cr)
 {
-	(void) cr;
 	vdev_file_t *vf;
 	zfs_file_t *fp;
 	zfs_file_attr_t zfa;
@@ -153,12 +153,12 @@ vdev_file_open(vdev_t *vd, uint64_t *psize, uint64_t *max_psize,
 	// Windows uses vdev_physpath if available
 	error = zfs_file_open(vd->vdev_physpath ?
 	    vd->vdev_physpath : vd->vdev_path,
-	    vdev_file_open_mode(spa_mode(vd->vdev_spa)), 0, kcred, &fp);
+	    vdev_file_open_mode(spa_mode(vd->vdev_spa)), 0, cr, &fp);
 #else
 	ASSERT3S(vd->vdev_path[0], ==, '/');
 
 	error = zfs_file_open(vd->vdev_path,
-	    vdev_file_open_mode(spa_mode(vd->vdev_spa)), 0, kcred, &fp);
+	    vdev_file_open_mode(spa_mode(vd->vdev_spa)), 0, cr, &fp);
 #endif
 	if (error) {
 		vd->vdev_stat.vs_aux = VDEV_AUX_OPEN_FAILED;
